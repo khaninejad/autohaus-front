6ec4ce13d5f60f14e55a0a40c58d90bbc6597b02
Payam <payam.khaninejad@coinhaven.com>
2023-01-26 12:27:24 +0100

    select department default value

--- src/pages/employee/edit-employee.js
+++ src/pages/employee/edit-employee.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Input, Button } from '@mui/material';
+import { Input, Button, MenuItem, FormControl } from '@mui/material';
 import configuration from '../../shared/config';
 import { useNavigate, useParams } from 'react-router-dom';
+import Select from "react-select";
 
 const EditEmployee = () => {
     const { id } = useParams();
-    const [employee, setEmployee] = useState({ first_name: '', last_name: '', address:'', job_title: '', department: '' });
+    const [employee, setEmployee] = useState({ first_name: '', last_name: '', address: '', job_title: '', department: '' });
+    const [options, setOptions] = useState([]);
     const [error, setError] = useState('');
     const token = localStorage.getItem('token');
     const navigate = useNavigate();
+
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -25,6 +29,21 @@ const EditEmployee = () => {
             }
         }
         fetchData();
+
+        const fetchOptions = async () => {
+            try {
+                const res = await axios.get(`${configuration().api_url}api/department`, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+                console.log(res);
+                setOptions(res.data.map(department => ({ value: department._id, label: department.name })));
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        fetchOptions();
     }, [token, id]);
 
     const handleSubmit = async (e) => {
@@ -45,6 +64,8 @@ const EditEmployee = () => {
         }
     }
 
+
+
     return (
         <form>
             {error && <p>{error}</p>}
@@ -52,6 +73,21 @@ const EditEmployee = () => {
             <Input label="Last Name" value={employee.last_name} onChange={e => setEmployee({ ...employee, last_name: e.target.value })} />
             <Input label="Address" value={employee.address} onChange={e => setEmployee({ ...employee, address: e.target.value })} />
             <Input label="job_title" value={employee.job_title} onChange={e => setEmployee({ ...employee, job_title: e.target.value })} />
+            <FormControl fullWidth>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    options={options}
+                    value={options.find(option => option.value === employee.department)}
+                    onChange={(selectedOption) => setEmployee({ ...employee, department: selectedOption.value })}
+                >
+                    {options.map((option) => (
+                        <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
             <Button onClick={handleSubmit}>Save</Button>
         </form>
     );
